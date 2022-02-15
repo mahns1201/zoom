e482d324111e6334609d5117963a061e4209ce72
서민혁 <mahns1201@gmail.com>
2022-02-16 00:14:52 +0900

    [feat] simple-chat

--- src/server.js
+++ src/server.js
@@ -15,14 +15,28 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on('connection', socket => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon';
+
   console.log('Connected to Browser ✅');
 
   socket.on('close', () => console.log('Disconnected from the Browser ❌'));
-  socket.on('message', message => {
-    console.log(message);
+
+  socket.on('message', msg => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach(aSocket =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`),
+        );
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
   });
-  socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
